024d2af59ba6ee86baee6c649fec7dd6146b0bdd
hashirniazi <hashirkhanniazii@gmail.com>
2026-05-03 02:30:12 +0500

    Refactored code into src/mandelbrot_io.cpp

--- include/mandelbrot.hpp
+++ include/mandelbrot.hpp
@@ -9,11 +9,9 @@ const int WIDTH = 1600;
 const int HEIGHT = 1200;
 const int MAX_ITER = 1000;
 
-struct Color {
-    unsigned char r, g, b;
-};
-
-void compute_mandelbrot(std::vector<int>& image);
+// Function declarations
+void compute_mandelbrot_parallel(std::vector<int>& image);
+void compute_mandelbrot_serial(std::vector<int>& image);
 void save_ppm(const std::vector<int>& image, const std::string& filename);
 
 #endif
